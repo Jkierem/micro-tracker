cd337dbba56cb9bc00ddaf5fddbb309ac7db1063
Jkierem <juanmiguelgomez8@gmail.com>
2024-08-11 13:23:32 -0500

    Basic sturcture for login

--- src/support/routing/views.ts
+++ src/support/routing/views.ts
@@ -1,11 +1,12 @@
 import { Data } from "effect";
 
 export type View = Data.TaggedEnum<{
+    Login: {},
     ImageViewer: {},
     MainMenu: {},
     Gallery: {},
     Archive: {},
 }>
 
-const { ImageViewer, MainMenu, Gallery, Archive } = Data.taggedEnum<View>();
-export const Views = { ImageViewer, MainMenu, Gallery, Archive };
+const { Login, ImageViewer, MainMenu, Gallery, Archive } = Data.taggedEnum<View>();
+export const Views = { Login, ImageViewer, MainMenu, Gallery, Archive };
