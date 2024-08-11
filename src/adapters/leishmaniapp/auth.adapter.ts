95ec3959dbe0da13bfcde2f88496bb35d640946b
Jkierem <juanmiguelgomez8@gmail.com>
2024-08-11 13:23:32 -0500

    Basic sturcture for login

--- src/adapters/leishmaniapp/auth.adapter.ts
+++ src/adapters/leishmaniapp/auth.adapter.ts
@@ -6,8 +6,13 @@ export class AuthError
 extends Data.TaggedError("AuthError")<{ error: unknown }> {}
 
 declare namespace AuthAdapter {
+    type Credentials = {
+        email: string,
+        password: string,
+    }
+
     type Shape = {
-        stuff: () => Effect.Effect<AuthResponse, AuthError>;
+        login: (credentials: Credentials) => Effect.Effect<AuthResponse, AuthError>;
     }
 }
 
@@ -17,13 +22,13 @@ extends Context.Tag("AuthAdapter")<
     AuthAdapter.Shape
 >() {
     static Live = Layer.succeed(AuthAdapter, AuthAdapter.of({
-        stuff() {
+        login({ email, password }) {
             return Effect.tryPromise({
                 try() {
                     const cl = new AuthServiceClient("");
                     const req = new AuthRequest();
-                    req.setEmail("whatever");
-                    req.setPassword("");
+                    req.setEmail(email);
+                    req.setPassword(password);
                     return cl.authenticate(req);
                 },
                 catch(error) {
