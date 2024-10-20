9071639205844a0b26142b984863ad2689ea48f4
Jkierem <juanmiguelgomez8@gmail.com>
2024-10-20 13:38:35 -0500

    Configured for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  worker:{
+    format: "es"
+  },
+  build: {
+    target: "ES2022",
+  },
   plugins: [react(), VitePWA({
     registerType: 'prompt',
     injectRegister: "script",
@@ -24,6 +30,7 @@ export default defineConfig({
       globPatterns: ['**/*'],
       cleanupOutdatedCaches: true,
       clientsClaim: true,
+      maximumFileSizeToCacheInBytes: 164_000_000,
     },
 
     includeAssets: [
